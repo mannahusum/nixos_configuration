facfdf0cfe2a1362ae0f6e441de506d6957e121b
Christian Albertsen <christian@wudika.de>
2021-07-17 22:11:47 +0200

    PianoBooster changes to actually compile and run

--- users/christian/pianobooster.nix
+++ users/christian/pianobooster.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, cmake, pkg-config, qttools
+{ lib, fetchFromGitHub, cmake, pkg-config, qttools
 , alsaLib, ftgl, libGLU, libjack2, qtbase, rtmidi,
 mkDerivation
 }:
@@ -22,7 +22,7 @@ mkDerivation rec {
     "-DOpenGL_GL_PREFERENCE=GLVND"
   ];
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "A MIDI file player that teaches you how to play the piano";
     homepage = "https://github.com/captnfab/PianoBooster";
     license = licenses.gpl3Plus;
