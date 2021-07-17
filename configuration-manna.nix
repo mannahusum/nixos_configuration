0b2fb14ea3889cfe898576499e5ab9a5e47b4b15
Christian Albertsen <christian@wudika.de>
2021-07-17 22:15:49 +0200

    Move configuration for different machines in their own files

---
+++ configuration-manna.nix
@@ -0,0 +1,21 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+{
+  imports =
+    [
+      ./computers/mannahusum/configuration.nix
+      ./configuration-gui.nix
+    ];
+
+  nixpkgs.overlays = [
+    (_: prev: {
+
+      prev.linuxPackagesFor = kernel:
+        (prev.linuxPackagesFor kernel).extend (_: _: { ati_drivers_x11 = null; });
+
+    })
+  ];
+}
