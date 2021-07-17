c2f7a37a9d125c6f50393014c3cc75d8be4697a0
Christian Albertsen <christian@wudika.de>
2021-07-17 22:15:26 +0200

    Configuration for a vm

---
+++ computers/vm/configuration.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, ... }:
+{
+  networking = {
+    hostName = "mannavm"; # Define your hostname.
+  };
+  virtualisation = {
+    diskSize = 8000; # MB
+    memorySize = 10240; # MB
+    # qemu.options = [
+    #   "-virtfs local,path=${mount_host_path},security_model=none,mount_tag=${mount_tag}"
+    # ];
+
+    # We don't want to use tmpfs, otherwise the nix store's size will be bounded
+    # by a fraction of available RAM.
+    writableStoreUseTmpfs = false;
+
+    # Because we want to test GRUB.
+    # This may require `system-features = kvm` in your `nix.conf`, and your user
+    # to be part of the `kvm` group, otherwise you may get:
+    #     Could not access KVM kernel module: Permission denied
+    useBootLoader = true;
+  };
+}
