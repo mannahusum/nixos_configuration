c85866d23768e122e150a63f964c156157029a1a
Christian Albertsen <christian@wudika.de>
2021-07-17 22:12:18 +0200

    Config for git

--- users/christian/ssh.nix
+++ users/christian/ssh.nix
@@ -17,16 +17,15 @@ in {
       "wudika" = mywudika;
       "*.wudika.de" = mywudika;
       "wudika.de" = mywudika;
+      "github.com" = {
+        forwardX11 = false;
+      };
     };
   };
   home = {
     activation.installSSHprivateKey = lib.hm.dag.entryAfter ["writeBoundary"] ''
       $DRY_RUN_CMD mkdir -p $HOME/.ssh/controlmasters
       $DRY_RUN_CMD install -D -m600 ${./private/id_rsa} $HOME/.ssh/id_rsa
-      if [ -z "$SSH_AUTH_SOCK" -o ! -r "$SSH_AUTH_SOCK" ] ; then
-        $DRY_RUN_CMD eval `ssh-agent -s`
-        $DRY_RUN_CMD ssh-add
-      fi
     '';
   };
 }
