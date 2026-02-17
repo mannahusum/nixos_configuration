e4b6d7244c7077a0eff421928f1088ee63439b18
Christian Albertsen <christian@wudika.de>
2026-02-17 23:49:06 +0100

    Make automaticrippingmachine temporarily work

--- modules/arm.nix
+++ modules/arm.nix
@@ -2,7 +2,6 @@
   config,
   lib,
   pkgs,
-  stdenv,
   ...
 }: let
   cfg = config.caarm;
@@ -34,51 +33,53 @@ in {
     };
 
     # Runtime
-    virtualisation.docker = {
-      enable = true;
-      autoPrune.enable = true;
-      daemon.settings = {
-        userland-proxy = false;
-        experimental = true;
-        ipv6 = true;
-        fixed-cidr-v6 = "fd00::/80";
+    virtualisation = {
+      docker = {
+        enable = true;
+        autoPrune.enable = true;
+        daemon.settings = {
+          userland-proxy = false;
+          experimental = true;
+          ipv6 = true;
+          fixed-cidr-v6 = "fd00::/80";
+        };
+      };
+      oci-containers.backend = "docker";
+      oci-containers.containers."arm-ripper" = {
+        pull = "always";
+        image = "automaticrippingmachine/automatic-ripping-machine:latest";
+        #ports = [ "8080:8080" ];
+        ports = [ "28982:8080" ];
+        environment = {
+          ARM_UID = "1000";
+          ARM_GID = "1001";
+          TZ = "Europe/Berlin";
+        };
+        volumes = [
+          "/mnt:/mnt"
+          "/media/arm:/home/arm"
+          "/etc/arm/config:/etc/arm/config"
+          "/media/audio/arm:/home/arm/Music"
+          "/var/log/arm:/home/arm/logs"
+          # "/media/arm/media:/home/arm/media"
+        ];
+        devices = [
+          "/dev/sr0:/dev/sr0"
+          "/dev/sr1:/dev/sr1"
+          "/dev/sr2:/dev/sr2"
+          "/dev/sr3:/dev/sr3"
+          "/dev/sg0:/dev/sg0"
+          "/dev/sg1:/dev/sg1"
+          "/dev/sg2:/dev/sg2"
+        ];
+        privileged = true;
+        autoStart = true;
       };
     };
-    virtualisation.oci-containers.backend = "docker";
 
     # Containers
     networking.firewall = {
       allowedTCPPorts = [ 28982 ];
-    };
-    virtualisation.oci-containers.containers."arm-ripper" = {
-      pull = "always";
-      image = "automaticrippingmachine/automatic-ripping-machine:latest";
-      #ports = [ "8080:8080" ];
-      ports = [ "28982:8080" ];
-      environment = {
-        ARM_UID = "1000";
-        ARM_GID = "1001";
-        TZ = "Europe/Berlin";
-      };
-      volumes = [
-        "/mnt:/mnt"
-        "/media/arm:/home/arm"
-        "/etc/arm/config:/etc/arm/config"
-        "/media/audio/arm:/home/arm/Music"
-        "/var/log/arm:/home/arm/logs"
-        # "/media/arm/media:/home/arm/media"
-      ];
-      devices = [
-        "/dev/sr0:/dev/sr0"
-        "/dev/sr1:/dev/sr1"
-        "/dev/sr2:/dev/sr2"
-        "/dev/sr3:/dev/sr3"
-        "/dev/sg0:/dev/sg0"
-        "/dev/sg1:/dev/sg1"
-        "/dev/sg2:/dev/sg2"
-      ];
-      privileged = true;
-      autoStart = true;
     };
   };
 }
