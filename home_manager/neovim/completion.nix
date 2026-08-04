de6166493ef0ecfa70005e02f7bf41495af4a5e7
to6338 <christian.albertsen@kit.edu>
2026-08-04 11:36:48 +0000

    add language servers for thing I regularily work on

--- home_manager/neovim/completion.nix
+++ home_manager/neovim/completion.nix
@@ -1,10 +1,18 @@
 { pkgs, ... }:
 {
+  imports = [
+    ./treesitter.nix
+  ];
+
   programs.neovim = {
     extraPackages = with pkgs; [
+      ansible-language-server
+      bash-language-server
       gh
       glab
       nixd
+      lua-language-server
+      pyright
       wordnet
     ];
 
@@ -30,16 +38,41 @@
                   async = true,
                   module = "blink-emoji",
                   name = "emoji",
-                  score_offset = 15,
+                  score_offset = 150,
                   opts = { insert = true, },
                 },
                 git = {
                   module = 'blink-cmp-git',
                   name = 'git',
-                  score_offset = 100,
+                  score_offset = 50,
                   opts = {
                     commit = { },
                   },
+                },
+                lsp = {
+                  name = 'LSP',
+                  module = 'blink.cmp.sources.lsp',
+                  score_offset = 200,
+                },
+                path = {
+                  name = 'Path',
+                  module = 'blink.cmp.sources.path',
+                  score_offset = 175,
+                  opts = {
+                    trailing_slash = true,
+                  },
+                },
+                buffer = {
+                  name = 'Buffer',
+                  module = 'blink.cmp.sources.buffer',
+                  min_keyword_length = 3,
+                  score_offset = 188,
+                },
+                snippets = {
+                  name = 'Snippets',
+                  module = 'blink.cmp.sources.snippets',
+                  min_keyword_length = 2,
+                  score_offset = 118,
                 },
               },
             },
@@ -49,14 +82,27 @@
       blink-cmp-dictionary
       blink-emoji-nvim
       blink-cmp-git
+      friendly-snippets
 
+      astrolsp
       {
         type = "lua";
         plugin = nvim-lspconfig;
         config = ''
           local lsp_capabilities = require('blink.cmp').get_lsp_capabilities()
-          vim.lsp.config('nixd', { capabilities = lsp_capabilities })
-          vim.lsp.enable('nixd')
+          vim.lsp.config('ansiblels', { capabilities = lsp_capabilities })
+          vim.lsp.config('bashls', { capabilities = lsp_capabilities })
+          vim.lsp.config('lua_ls', { capabilities = lsp_capabilities })
+          vim.lsp.config('nixd', {
+            capabilities = lsp_capabilities,
+          })
+          vim.lsp.config('powershell_es', {
+            bundle_path = '${pkgs.powershell-editor-services.out}',
+            shell = '${pkgs.powershell}/bin/pwsh',
+            capabilities = lsp_capabilities
+          })
+          vim.lsp.config('pyright', { capabilities = lsp_capabilities })
+          vim.lsp.enable({'ansiblels', 'bashls', 'lua_ls', 'nixd', 'powershell_es', 'pyright'})
         '';
       }
     ];
